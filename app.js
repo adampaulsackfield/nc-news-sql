ce6bd7130551d2ca1e856ec805bda830c0e7b7f2
Adam Sackfield <adamsackfield20@gmail.com>
2022-03-28 11:45:36 +0100

    Completed GET /api/topics

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const { getTopics } = require('./controllers/topic.controller');
+
+app.use(express.json());
+
+app.get('/api/topics', getTopics);
+
+app.all('*', (req, res) => {
+	res.status(404).send({ message: 'Path not found' });
+});
+
+app.use((err, req, res, next) => {
+	if (err.status && err.msg) {
+		res.status(err.status).send({ message: err.msg });
+	} else {
+		next(err);
+	}
+});
+
+app.use((err, req, res, next) => {
+	console.error(err);
+	res.status(500).send({ message: 'Internal server error' });
+});
+
+module.exports = app;
