53e22f852deb1635daff06e1c067fdb2fd5211d3
Adam Sackfield <adamsackfield20@gmail.com>
2022-03-31 17:04:39 +0100

    remove .only from endpoint test

--- __tests__/endpoint.test.js
+++ __tests__/endpoint.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 const fs = require('fs/promises');
 
-describe.only('ENDPOINT.JSON', () => {
+describe('ENDPOINT.JSON', () => {
 	describe('GET /api', () => {
 		it('should return a status of 200 and a JSON object for our endpoints', async () => {
 			let expected = await fs.readFile(`./endpoints.json`, 'utf-8');
