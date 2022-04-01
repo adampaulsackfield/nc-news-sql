1db602fc252e5e0c53fecb24b4ea5f7172611a7b
Adam Sackfield <adamsackfield20@gmail.com>
2022-04-01 12:32:44 +0100

    add getUserByUsername

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -27,5 +27,30 @@ describe('USERS', () => {
 		});
 	});
 
-	describe('GET /api/users/:username', () => {});
+	describe('GET /api/users/:username', () => {
+		it('should return a user object if given a username that exists', () => {
+			const expected = {
+				username: 'lurker',
+				name: 'do_nothing',
+				avatar_url:
+					'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+			};
+
+			return request(app)
+				.get(`${ENDPOINT}/lurker`)
+				.expect(200)
+				.then((res) => {
+					expect(res.body.user).toEqual(expected);
+				});
+		});
+
+		it('should return a 404 status when the username does not exist', () => {
+			return request(app)
+				.get(`${ENDPOINT}/iDoNotExist`) // this also works for numbers
+				.expect(404)
+				.then((res) => {
+					expect(res.body.message).toBe('User does not exist');
+				});
+		});
+	});
 });
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,7 +1,11 @@
-const { getUsers } = require('../controllers/user.controller');
+const {
+	getUsers,
+	getUserByUsername,
+} = require('../controllers/user.controller');
 
 const userRouter = require('express').Router();
 
 userRouter.route('/').get(getUsers);
+userRouter.route('/:username').get(getUserByUsername);
 
 module.exports = userRouter;
