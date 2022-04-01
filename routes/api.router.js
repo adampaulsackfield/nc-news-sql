e934dfdfd41eaf9fe6ea0ae3f064ff6d203ee51b
Adam Sackfield <adamsackfield20@gmail.com>
2022-04-01 12:00:22 +0100

    add untracked file

--- routes/api.router.js
+++ routes/api.router.js
@@ -4,6 +4,10 @@ const articleRouter = require('./article.router');
 const commentRouter = require('./comment.router');
 const topicRouter = require('./topic.router');
 
+const { getEndpoints } = require('../controllers/endpoints.controller');
+
+apiRouter.get('/', getEndpoints);
+
 apiRouter.use('/users', userRouter);
 apiRouter.use('/articles', articleRouter);
 apiRouter.use('/comments', commentRouter);
