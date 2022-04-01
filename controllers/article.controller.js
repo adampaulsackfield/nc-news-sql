f8344d872139e36e5f70c142befe8b37ccd5785d
Adam Sackfield <adamsackfield20@gmail.com>
2022-04-01 16:03:07 +0100

    Refactor for async await and structured SQL queries cont

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -7,38 +7,41 @@ const {
 	postArticle,
 } = require('../models/article.model');
 
-exports.getArticles = (req, res, next) => {
+exports.getArticles = async (req, res, next) => {
 	const { sort_by, order, topic } = req.query;
 
-	selectArticles(sort_by, order, topic)
-		.then((articles) => {
-			res.status(200).send({ articles });
-		})
-		.catch((err) => next(err));
+	try {
+		const articles = await selectArticles(sort_by, order, topic);
+		res.status(200).send({ articles });
+	} catch (err) {
+		next(err);
+	}
 };
 
-exports.getArticleById = (req, res, next) => {
+exports.getArticleById = async (req, res, next) => {
 	const { article_id } = req.params;
 
-	selectArticleById(article_id)
-		.then((article) => {
-			res.status(200).send({ article });
-		})
-		.catch((err) => next(err));
+	try {
+		const article = await selectArticleById(article_id);
+		res.status(200).send({ article });
+	} catch (err) {
+		next(err);
+	}
 };
 
-exports.updateArticleById = (req, res, next) => {
+exports.updateArticleById = async (req, res, next) => {
 	const { article_id } = req.params;
 	const { inc_votes } = req.body;
 
-	patchArticleById(article_id, inc_votes)
-		.then((article) => {
-			res.status(200).send({ article });
-		})
-		.catch((err) => next(err));
+	try {
+		const article = await patchArticleById(article_id, inc_votes);
+		res.status(200).send({ article });
+	} catch (err) {
+		next(err);
+	}
 };
 
-exports.getCommentsByArticleId = (req, res, next) => {
+exports.getCommentsByArticleId = async (req, res, next) => {
 	const { article_id } = req.params;
 
 	const parsedArticleId = parseInt(article_id);
@@ -50,35 +53,36 @@ exports.getCommentsByArticleId = (req, res, next) => {
 		});
 	}
 
-	selectCommentsByArticleId(parsedArticleId)
-		.then((comments) => {
-			res.status(200).send({ comments });
-		})
-		.catch((err) => next(err));
+	try {
+		const comments = await selectCommentsByArticleId(parsedArticleId);
+		res.status(200).send({ comments });
+	} catch (err) {
+		next(err);
+	}
 };
 
-exports.addComment = (req, res, next) => {
+exports.addComment = async (req, res, next) => {
 	const { article_id } = req.params;
 	const reqBody = req.body;
 
-	postComment(article_id, reqBody)
-		.then((comment) => {
-			res.status(201).send({ comment });
-		})
-		.catch((err) => {
-			if (err.code === '23503') {
-				return next({ status: 404, msg: 'Article not found' });
-			}
-			next(err);
-		});
+	try {
+		const comment = await postComment(article_id, reqBody);
+		res.status(201).send({ comment });
+	} catch (err) {
+		if (err.code === '23503') {
+			return next({ status: 404, msg: 'Article not found' });
+		}
+		next(err);
+	}
 };
 
-exports.addArticle = (req, res, next) => {
+exports.addArticle = async (req, res, next) => {
 	const { author, body, title, topic } = req.body;
 
-	postArticle(author, body, title, topic)
-		.then((article) => {
-			res.status(201).send({ article });
-		})
-		.catch((err) => next(err));
+	try {
+		const article = await postArticle(author, body, title, topic);
+		res.status(201).send({ article });
+	} catch (err) {
+		next(err);
+	}
 };
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,19 +1,21 @@
 const { selectUsers, selectUserByUsername } = require('../models/user.model');
 
-exports.getUsers = (req, res, next) => {
-	selectUsers()
-		.then((users) => {
-			res.status(200).send({ users });
-		})
-		.catch((err) => next(err));
+exports.getUsers = async (req, res, next) => {
+	try {
+		const users = await selectUsers();
+		res.status(200).send({ users });
+	} catch (err) {
+		next(err);
+	}
 };
 
-exports.getUserByUsername = (req, res, next) => {
+exports.getUserByUsername = async (req, res, next) => {
 	const { username } = req.params;
 
-	selectUserByUsername(username)
-		.then((user) => {
-			res.status(200).send({ user });
-		})
-		.catch((err) => next(err));
+	try {
+		const user = await selectUserByUsername(username);
+		res.status(200).send({ user });
+	} catch (err) {
+		next(err);
+	}
 };
