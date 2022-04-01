8f4942f63b51e154c6d9dd92ed00a6672fb37fdb
Adam Sackfield <adamsackfield20@gmail.com>
2022-04-01 10:48:50 +0100

    change let to const inside endpoin.controller

--- controllers/endpoints.controller.js
+++ controllers/endpoints.controller.js
@@ -1,7 +1,7 @@
 const fs = require('fs/promises');
 
 exports.getEndpoints = async (req, res, next) => {
-	let endpoints = await fs.readFile(`./endpoints.json`, 'utf-8');
+	const endpoints = await fs.readFile(`./endpoints.json`, 'utf-8');
 
 	res.status(200).send({ endpoints });
 };
